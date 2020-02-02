8feef254a809c39757019fe1cd188507f9c8558a
yaoyuqi888 <504786993@qq.com>
2020-02-02 11:40:26 +0800

    first commit

---
+++ jump7.py
@@ -0,0 +1,8 @@
+echo "# Louplus" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/yaoyuqi888/Louplus.git
+git push -u origin master
+
+
